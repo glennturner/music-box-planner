8485b15d38b4a19942ba9a9cb9d6642c7187fa5a
Glenn Turner <glenn@standalone.com>
2019-08-23 17:56:21 -0500

    Removed debug output. Whitespace tweaks.

--- fancy-settings/settings.js
+++ fancy-settings/settings.js
@@ -1,11 +1,8 @@
 
 window.addEvent("domready", function () {
-    // Option 1: Use the manifest:
+		// Option 1: Use the manifest:
 	new FancySettings.initWithManifest(function ( settings ) {
-        settings.manifest.saveAccount.addEvent("action", function ( e ) {
-			console.log( settings );
-			console.log( settings.manifest.domain.element.value );
-			
+			settings.manifest.saveAccount.addEvent("action", function ( e ) {
 			function toggleDisplay( ele ) {
 				if ( ele.style.display == 'none' ) {
 					ele.style.display = 'block';
@@ -13,33 +10,33 @@ window.addEvent("domready", function () {
 					ele.style.display = 'none';
 				}
 			};
-			
+
 			// test Tender account
-			
-			var tender = new Tender({ 
+
+			var tender = new Tender({
 				domain: settings.manifest.domain.element.value,
-				token: settings.manifest.token.element.value 
+				token: settings.manifest.token.element.value
 			});
-			
+
 			var saveButton = document.querySelector( "input[value='Save Changes']" );
 			toggleDisplay( saveButton );
-			
+
 			// insert progress bar
 			var discussion_id = 'extratender-discussion';
 			var progress = document.getElementById( discussion_id );
-			
+
 			if ( !progress ) {
 				progress = document.createElement( 'div' );
 				progress.setAttribute( 'id', discussion_id );
-				
+
 				saveButton.parentNode.appendChild( progress );
-			}			
-			
+			}
+
 			progress.innerHTML = '<span id="extratender-acct-loader" class="ajaxloader"></span> <span id="extratender-progress-text">Verifying Account...</span>';
 			var loader = document.getElementById( 'extratender-acct-loader' );
 			loader.style.display = 'inline-block';
 			var progressText = document.getElementById( 'extratender-progress-text' );
-			
+
 			tender.getUsers( function ( json ) {
 				toggleDisplay( saveButton );
 				toggleDisplay( loader );
@@ -51,10 +48,10 @@ window.addEvent("domready", function () {
 				toggleDisplay( loader );
 				progressText.className = 'error';
 				progressText.innerHTML = 'Could not verify your account.';
-				console.log( 'ERROR' );
-				console.log( error );
+				console.error( 'ERROR' );
+				console.error( error );
 			});
-			
-        });
-    });
+
+				});
+		});
 });
